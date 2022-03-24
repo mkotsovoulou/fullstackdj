0bd6085bc9f9d43f63c6c7588dfcbe8257e88aa7
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-03-24 14:27:24 +0200

    changed settings

--- app/conference/settings.py
+++ app/conference/settings.py
@@ -18,7 +18,6 @@ from pathlib import Path
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -80,7 +79,7 @@ WSGI_APPLICATION = 'conference.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3")
     }
 }
 
