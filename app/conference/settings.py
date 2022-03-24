bf2e9008efe35d4312f7a16a0fad5ae43f48fdb5
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-03-24 13:49:51 +0200

    import os and heroku

--- app/conference/settings.py
+++ app/conference/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 """
 Django settings for conference project.
 
@@ -9,7 +10,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
