400767035eea9bb44cce1e37148ba14d2950194b
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-03-24 14:16:32 +0200

    added procfile and requirements

--- app/conference/urls.py
+++ app/conference/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     #path ("", views.index, name="index"),
     path('fullstack/', include('fullstack.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
